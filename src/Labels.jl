1e689b6f301a3a7873e9c48c0c02fc96c6472775
mwsohn <mwsohn@gmail.com>
2023-10-12 19:21:12 -0400

    Update Labels.jl

--- src/Labels.jl
+++ src/Labels.jl
@@ -318,7 +318,7 @@ end
 function value_label(_df::AbstractDataFrame, varnames::AbstractVector)
     valdict = value_dict(_df) 
     lname = value_key(_df,varnames)
-    if valdict == nothing || length(lname) == 0
+    if valdict == nothing || lname == nothing
         return nothing
     end
     vdict = Dict(x => valdict[lname[x]] for x in keys(lname))
